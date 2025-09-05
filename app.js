51006aa5c1a98aa694c2c1f2fee7ff3d77e6f206
abhishekthakur2121 <abhithakur2700@gmail.com>
2025-09-06 00:52:38 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app= express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser())
+app.use(cookieParser());
+const path = require('path')
+app.use(express.static(path.join(__dirname , "public")))
+
 
 app.set('view engine','ejs');
 
