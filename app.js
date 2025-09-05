c75048c0c9d7dd25e214009c6f0b8b986a1a297c
abhishekthakur2121 <abhithakur2700@gmail.com>
2025-09-06 00:21:22 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app= express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser())
+
+app.set('view engine','ejs');
+
+
+
+
+app.get('/', (req, res)=>{
+    res.send('app is working well')
+})
+
+app.listen(5000,()=>{
+    console.log('running at port number 5000')
+})
