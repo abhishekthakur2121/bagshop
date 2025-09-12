69e246d9691e92ebb9de6a2780d4c568c8772626
abhishekthakur2121 <abhithakur2700@gmail.com>
2025-09-13 03:18:02 +0530

    setting app

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.use((req, res, next) => {
   next();
 });
 
-
-app.listen(5000,()=>{
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
     console.log('running at port number 5000')
 })
 
